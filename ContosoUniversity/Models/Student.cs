60e47c58a19ccd65af50ba16810cd0d10ec2f858
Grzegorz Kudlacz <gkudel@gmail.com>
2015-05-20 15:41:52 +0200

    Date

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -9,11 +9,16 @@ namespace ContosoUniversity.Models
     public class Student
     {
         public int ID { get; set; }
+        
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
+
         [Display(Name = "First Name")]
         public string FirstMidName { get; set; }
+
         [Display(Name  = "Enrollment Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime EnrollmentDate { get; set; }
 
         public virtual ICollection<Enrollment> Enrollments { get; set; }
